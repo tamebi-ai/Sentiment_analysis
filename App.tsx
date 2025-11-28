8f751cd108214dc9c3aed47c79901cfc034b94cb
carmelle16 <ahouandjinoucarmelle66@gmail.com>
2025-11-28 01:59:05 +0100

    Initialize App component with CommentAnalyzer

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CommentAnalyzer from './Pages/CommentAnalyzer';
+import './index.css';
+
+function App() {
+  return <CommentAnalyzer />;
+}
+
+export default App;
